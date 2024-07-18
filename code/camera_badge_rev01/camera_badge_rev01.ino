7345191a6ce6dfacc34942867c3d920eb3fc7779
jbohack <joebohack@gmail.com>
2024-07-18 13:44:46 -0400

    pushed main screen idle effect.

--- code/camera_badge_rev01/camera_badge_rev01.ino
+++ code/camera_badge_rev01/camera_badge_rev01.ino
@@ -165,8 +165,6 @@ int LEDDelayTime = 20;
 // 1 = basic debug info once per main loop
 // 2 = extra debug info
 int DebugSerial = 2;
-// Store a random number
-int MyRandom = 0;
 
 // //////////////////////////////////////////////////
 //
@@ -302,23 +300,7 @@ void loop()
       // RUN A RANDOM SCREEN UPDATE ONCE PER GROUP
       if (pos == 0)
       {
-        MyRandom = random(1, 5);
-        if (MyRandom == 1)
-        {
-          testFilledCircles(10, GC9A01A_BLUE);
-        }
-        if (MyRandom == 2)
-        {
-          testFilledRects(GC9A01A_YELLOW, GC9A01A_MAGENTA);
-        }
-        if (MyRandom == 3)
-        {
-          testTriangles();
-        }
-        if (MyRandom >= 4)
-        {
-          testTCHvalueText();
-        }
+        displayEffect();
       }
       //
       // LED FUNCTIONS
@@ -330,26 +312,9 @@ void loop()
     {
       pos = pos - 85;
       //
-      // RUN A RANDOM SCREEN UPDATE ONCE PER GROUP
       if (pos == 0)
       {
-        MyRandom = random(1, 5);
-        if (MyRandom == 1)
-        {
-          testFastLines(GC9A01A_RED, GC9A01A_BLUE);
-        }
-        if (MyRandom == 2)
-        {
-          testRects(GC9A01A_GREEN);
-        }
-        if (MyRandom == 3)
-        {
-          testTCHvalueText();
-        }
-        if (MyRandom >= 4)
-        {
-          testFilledTriangles();
-        }
+          displayEffect();
       }
       //
       // LED FUNCTIONS
@@ -361,27 +326,6 @@ void loop()
     {
       pos = pos - 170;
       //
-      // RUN A RANDOM SCREEN UPDATE ONCE PER GROUP
-      if (pos == 0)
-      {
-        MyRandom = random(1, 5);
-        if (MyRandom == 1)
-        {
-          testRoundRects();
-        }
-        if (MyRandom == 2)
-        {
-          testTCHvalueText();
-        }
-        if (MyRandom == 3)
-        {
-          testCircles(10, GC9A01A_RED);
-        }
-        if (MyRandom >= 4)
-        {
-          testFilledRoundRects();
-        }
-      }
       //
       // LED FUNCTIONS
       title_neo_colorshift(pos, 3);
@@ -406,8 +350,6 @@ void loop()
       Serial.print(i);
       Serial.print(" Pos=");
       Serial.print(pos);
-      Serial.print(" MyRandom=");
-      Serial.print(MyRandom);
     }
 
     //
@@ -1008,298 +950,47 @@ void status_neo_colorset(uint8_t mode)
 //
 // DISPLAY Functions
 // //////////////////////////////////////////////////
-unsigned long testTCHvalueText()
-{
-  tft.fillScreen(GC9A01A_BLACK);
-  unsigned long start = micros();
-  tft.setCursor(35, 60);
-  tft.setTextColor(GC9A01A_WHITE);
-  tft.setTextSize(3);
-  tft.println("TCH Values");
-  tft.setTextColor(GC9A01A_BLUE);
-  tft.setTextSize(2);
-  tft.print("  TCH01 ");
-  tft.setTextColor(GC9A01A_WHITE);
-  tft.print(Touch01_Value);
-  tft.setTextColor(GC9A01A_BLUE);
-  tft.print("/");
-  tft.println(Touch01_Threshold);
-  tft.print("  TCH03 ");
-  tft.setTextColor(GC9A01A_WHITE);
-  tft.print(Touch03_Value);
-  tft.setTextColor(GC9A01A_BLUE);
-  tft.print("/");
-  tft.println(Touch03_Threshold);
-  tft.print("  TCH05 ");
-  tft.setTextColor(GC9A01A_WHITE);
-  tft.print(Touch05_Value);
-  tft.setTextColor(GC9A01A_BLUE);
-  tft.print("/");
-  tft.println(Touch05_Threshold);
-  return micros() - start;
-}
-//
-unsigned long testFillScreen()
-{
-  unsigned long start = micros();
-  tft.fillScreen(GC9A01A_BLACK);
-  yield();
-  tft.fillScreen(GC9A01A_RED);
-  yield();
-  tft.fillScreen(GC9A01A_GREEN);
-  yield();
-  tft.fillScreen(GC9A01A_BLUE);
-  yield();
-  tft.fillScreen(GC9A01A_BLACK);
-  yield();
-  return micros() - start;
-}
-//
-unsigned long testText()
-{
-  tft.fillScreen(GC9A01A_BLACK);
-  unsigned long start = micros();
-  tft.setCursor(0, 0);
-  tft.setTextColor(GC9A01A_WHITE);
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(GC9A01A_YELLOW);
-  tft.setTextSize(2);
-  tft.println(1234.56);
-  tft.setTextColor(GC9A01A_RED);
-  tft.setTextSize(3);
-  tft.println(0xDEADBEEF, HEX);
-  tft.println();
-  tft.setTextColor(GC9A01A_GREEN);
-  tft.setTextSize(5);
-  tft.println("Groop");
-  tft.setTextSize(2);
-  tft.println("I implore thee,");
-  tft.setTextSize(1);
-  tft.println("my foonting turlingdromes.");
-  tft.println("And hooptiously drangle me");
-  tft.println("with crinkly bindlewurdles,");
-  tft.println("Or I will rend thee");
-  tft.println("in the gobberwarts");
-  tft.println("with my blurglecruncheon,");
-  tft.println("see if I don't!");
-  return micros() - start;
-}
-//
-unsigned long testLines(uint16_t color)
-{
-  unsigned long start, t;
-  int x1, y1, x2, y2,
-      w = tft.width(),
-      h = tft.height();
-  tft.fillScreen(GC9A01A_BLACK);
-  yield();
-  x1 = y1 = 0;
-  y2 = h - 1;
-  start = micros();
-  for (x2 = 0; x2 < w; x2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  x2 = w - 1;
-  for (y2 = 0; y2 < h; y2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  t = micros() - start; // fillScreen doesn't count against timing
-  yield();
-  tft.fillScreen(GC9A01A_BLACK);
-  yield();
-  x1 = w - 1;
-  y1 = 0;
-  y2 = h - 1;
-  start = micros();
-  for (x2 = 0; x2 < w; x2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  x2 = 0;
-  for (y2 = 0; y2 < h; y2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  t += micros() - start;
-  yield();
-  tft.fillScreen(GC9A01A_BLACK);
-  yield();
-  x1 = 0;
-  y1 = h - 1;
-  y2 = 0;
-  start = micros();
-  for (x2 = 0; x2 < w; x2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  x2 = w - 1;
-  for (y2 = 0; y2 < h; y2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  t += micros() - start;
-  yield();
-  tft.fillScreen(GC9A01A_BLACK);
-  yield();
-  x1 = w - 1;
-  y1 = h - 1;
-  y2 = 0;
-  start = micros();
-  for (x2 = 0; x2 < w; x2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  x2 = 0;
-  for (y2 = 0; y2 < h; y2 += 6)
-    tft.drawLine(x1, y1, x2, y2, color);
-  yield();
-  return micros() - start;
-}
-//
-unsigned long testFastLines(uint16_t color1, uint16_t color2)
-{
-  unsigned long start;
-  int x, y, w = tft.width(), h = tft.height();
-  tft.fillScreen(GC9A01A_BLACK);
-  start = micros();
-  for (y = 0; y < h; y += 5)
-    tft.drawFastHLine(0, y, w, color1);
-  for (x = 0; x < w; x += 5)
-    tft.drawFastVLine(x, 0, h, color2);
-  return micros() - start;
-}
-//
-unsigned long testRects(uint16_t color)
-{
-  unsigned long start;
-  int n, i, i2,
-      cx = tft.width() / 2,
-      cy = tft.height() / 2;
-  tft.fillScreen(GC9A01A_BLACK);
-  n = min(tft.width(), tft.height());
-  start = micros();
-  for (i = 2; i < n; i += 6)
-  {
-    i2 = i / 2;
-    tft.drawRect(cx - i2, cy - i2, i, i, color);
-  }
-  return micros() - start;
-}
-//
-unsigned long testFilledRects(uint16_t color1, uint16_t color2)
-{
-  unsigned long start, t = 0;
-  int n, i, i2,
-      cx = tft.width() / 2 - 1,
-      cy = tft.height() / 2 - 1;
-  tft.fillScreen(GC9A01A_BLACK);
-  n = min(tft.width(), tft.height());
-  for (i = n; i > 0; i -= 6)
-  {
-    i2 = i / 2;
-    start = micros();
-    tft.fillRect(cx - i2, cy - i2, i, i, color1);
-    t += micros() - start;
-    // Outlines are not included in timing results
-    tft.drawRect(cx - i2, cy - i2, i, i, color2);
-    yield();
-  }
-  return t;
-}
-//
-unsigned long testFilledCircles(uint8_t radius, uint16_t color)
-{
-  unsigned long start;
-  int x, y, w = tft.width(), h = tft.height(), r2 = radius * 2;
-  tft.fillScreen(GC9A01A_BLACK);
-  start = micros();
-  for (x = radius; x < w; x += r2)
-  {
-    for (y = radius; y < h; y += r2)
-    {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-  return micros() - start;
-}
-//
-unsigned long testCircles(uint8_t radius, uint16_t color)
-{
-  unsigned long start;
-  int x, y, r2 = radius * 2,
-            w = tft.width() + radius,
-            h = tft.height() + radius;
-  // Screen is not cleared for this one -- this is
-  // intentional and does not affect the reported time.
-  start = micros();
-  for (x = 0; x < w; x += r2)
-  {
-    for (y = 0; y < h; y += r2)
-    {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-  return micros() - start;
-}
-//
-unsigned long testTriangles()
-{
-  unsigned long start;
-  int n, i, cx = tft.width() / 2 - 1,
-            cy = tft.height() / 2 - 1;
-  tft.fillScreen(GC9A01A_BLACK);
-  n = min(cx, cy);
-  start = micros();
-  for (i = 0; i < n; i += 5)
-  {
-    tft.drawTriangle(
-        cx, cy - i,     // peak
-        cx - i, cy + i, // bottom left
-        cx + i, cy + i, // bottom right
-        tft.color565(i, i, i));
-  }
-  return micros() - start;
-}
-//
-unsigned long testFilledTriangles()
+unsigned long displayEffect()
 {
   unsigned long start, t = 0;
   int i, cx = tft.width() / 2 - 1,
          cy = tft.height() / 2 - 1;
   tft.fillScreen(GC9A01A_BLACK);
   start = micros();
+
+  uint16_t startColor = tft.color565(255, 0, 0);
+  uint16_t endColor = tft.color565(0, 0, 255);
+
+  int colorSteps = min(cx, cy) / 5;
+  
   for (i = min(cx, cy); i > 10; i -= 5)
   {
     start = micros();
+    
+    uint8_t red = map(i, 10, min(cx, cy), 255, 0);
+    uint8_t green = 0;
+    uint8_t blue = map(i, 10, min(cx, cy), 0, 255);
+    
     tft.fillTriangle(cx, cy - i, cx - i, cy + i, cx + i, cy + i,
-                     tft.color565(0, i * 10, i * 10));
-    t += micros() - start;
+                     tft.color565(red, green, blue));
+
     tft.drawTriangle(cx, cy - i, cx - i, cy + i, cx + i, cy + i,
-                     tft.color565(i * 10, i * 10, 0));
+                     tft.color565(0, 255, 255));
+
+    uint8_t invertedRed = map(i, 10, min(cx, cy), 0, 255);
+    uint8_t invertedBlue = map(i, 10, min(cx, cy), 255, 0);
+    
+    tft.fillTriangle(cx, cy + i, cx - i, cy - i, cx + i, cy - i,
+                     tft.color565(invertedRed, green, invertedBlue));
+
+    tft.drawTriangle(cx, cy + i, cx - i, cy - i, cx + i, cy - i,
+                     tft.color565(0, 255, 255));
+
+    t += micros() - start;
     yield();
   }
+
+  tft.fillTriangle(cx, cy - 10, cx - 10, cy + 10, cx + 10, cy + 10, GC9A01A_BLACK);
+
   return t;
-}
-//
-unsigned long testRoundRects()
-{
-  unsigned long start;
-  int w, i, i2,
-      cx = tft.width() / 2 - 1,
-      cy = tft.height() / 2 - 1;
-  tft.fillScreen(GC9A01A_BLACK);
-  w = min(tft.width(), tft.height());
-  start = micros();
-  for (i = 0; i < w; i += 6)
-  {
-    i2 = i / 2;
-    tft.drawRoundRect(cx - i2, cy - i2, i, i, i / 8, tft.color565(i, 0, 0));
-  }
-  return micros() - start;
-}
-//
-unsigned long testFilledRoundRects()
-{
-  unsigned long start;
-  int i, i2,
-      cx = tft.width() / 2 - 1,
-      cy = tft.height() / 2 - 1;
-  tft.fillScreen(GC9A01A_BLACK);
-  start = micros();
-  for (i = min(tft.width(), tft.height()); i > 20; i -= 6)
-  {
-    i2 = i / 2;
-    tft.fillRoundRect(cx - i2, cy - i2, i, i, i / 8, tft.color565(0, i, 0));
-    yield();
-  }
-  return micros() - start;
 }
